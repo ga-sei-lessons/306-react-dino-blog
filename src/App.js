f8ba3369f242107de27a312ea6c7d9444d274481
weston-bailey <wdbailey87@gmail.com>
2023-04-18 16:13:59 -0700

    finish blog

--- src/App.js
+++ src/App.js
@@ -1,27 +1,92 @@
 import React, { Component } from "react"
 import Post from "./Post"
+import { starterPosts } from "./copy"
 
 // export defualt and define a class on the same line
 export default class App extends Component {
+  state = {
+    posts: starterPosts,
+    author: '',
+    title: '',
+    body: ''
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log('submit the form!')
+    this.setState(prevState => {
+      return {
+        posts: [...prevState.posts, {
+          author: prevState.author,
+          title: prevState.title,
+          body: prevState.body,
+          comments: []
+        }],
+        author: '',
+        title: '',
+        body: ''
+      }
+    })
+  }
+
+  handleFormInput = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
   render() {
-    // define some starter post data
-    const post = {
-      title: "Dinos are Cool",
-      author: "Stealth Stegosaurus",
-      body: "I like dinos, because they are neat.",
-      comments: ["🔥", "So brave 👏", "100% agree"]
-    }
-
-    return (
-      <div>
-        <h1>React Dino Blog 🦖</h1>
-
+    const postComponents = this.state.posts.map((post, i) => {
+      return (
         <Post 
           title={post.title}
           author={post.author}
           body={post.body}
           comments={post.comments}
+          key={`post ${i}`}
         />
+      )
+    })
+
+    return (
+      <div>
+        <h1>React Dino Blog 🦖</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="author">Author:</label>
+            <input 
+              type="text"
+              id="author"
+              placeholder="your name..."
+              onChange={this.handleFormInput}
+              value={this.state.author}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="title">Post Title:</label>
+            <input 
+              type="text"
+              id="title"
+              placeholder="Post Title...."
+              onChange={this.handleFormInput}
+              value={this.state.title}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="body">Post Body:</label>
+            <input 
+              type="text"
+              id="body"
+              placeholder="Enter Your Post"
+              onChange={this.handleFormInput}
+              value={this.state.body}
+            />
+          </div>
+          <button type="submit">Create Post</button>
+        </form>
+        {postComponents}
       </div>
     )
   }
